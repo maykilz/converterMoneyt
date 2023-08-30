16709a21c45cdfa484dfefab99b625ea412159e9
mayklz <mayil_10@mail.ru>
2023-08-30 11:07:42 +0300

    Конвертер валют

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Converter } from './components/converter/Converter';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [courseMoney, setCourseMoney] = useState('');
+
+  useEffect(()=> {
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js').then((rsp)=> { return rsp.json()})
+    .then((rsp)=> setCourseMoney(rsp.Valute));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Converter valutes={courseMoney}/> 
+
     </div>
   );
 }
---
+++ src/components/converter/Converter.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react'
+import './Converter.scss';
+
+export  const  Converter = (props) =>{ 
+  var USDConvert;
+  var EURConvert; 
+
+   if ( props.valutes!=='')  {
+    USDConvert = props.valutes.USD.Value; 
+    EURConvert = props.valutes.EUR.Value; 
+ 
+    
+  }
+  else {
+    USDConvert ='Сервер не ответил'; 
+    EURConvert= 'Сервер не ответил';  
+  }
+
+
+  const [toValute, changeToValute]  = useState('RUB');
+
+   const [fromValute, changeFromValute]  = useState('USD');
+   const [toValue, changeToValue]  = useState(0);  
+    const [fromValue, changeFromValue]  = useState(0); 
+      
+    const ConverteTorFnc = (toVal, fromVal, toValue, fromValue) => { 
+      switch (toVal) {
+        case 'RUB':
+          switch (fromVal) {
+            case 'USD': 
+              console.log(toValue);
+              changeFromValue(toValue/USDConvert);
+              break;
+            case 'RUB':  
+              changeFromValue(toValue);
+              break;
+            case 'EUR': 
+              console.log(toValue);
+              changeFromValue(toValue/EURConvert);
+              break;
+            default:
+              break;
+          }
+          break; // Добавьте break здесь
+        case 'USD':
+          switch (fromVal) {
+            case 'RUB': 
+              console.log(toValue);
+              changeFromValue(USDConvert*toValue);
+              break;
+            case 'USD':  
+              changeFromValue(toValue);
+              break;
+            case 'EUR':
+              var valueConvert =   USDConvert/EURConvert;  
+              var finalValue = toValue*valueConvert;
+
+              changeFromValue(finalValue);
+              break;
+            default:
+              break;
+          }
+          break; // Добавьте break здесь
+        default:
+          break;
+      }
+      
+    
+      
+   }
+
+  return ( 
+    <div>  
+      
+      Актуальный курс валют:  <br /> <br />
+
+      USD: {USDConvert} <br />
+      EUR:  {EURConvert}
+        <div className="converterblock"> 
+            <select name="" id="" onChange={(evt)=> {
+              changeToValute(evt.target.value); 
+              ConverteTorFnc(toValute, fromValute, toValue, fromValue); 
+
+            }}>
+                <option value="RUB">RUB</option>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+            </select>
+            <input type="text" className='toconvert'  value={toValue} onChange={(evt)=>{
+              changeToValue(evt.target.value); 
+              ConverteTorFnc(toValute, fromValute, evt.target.value, fromValue); 
+            }}/> 
+
+
+            <img src="images/lefttoright.jpg" alt="" className='arrow' />
+               <select name="" id="" onChange={(evt)=> {
+                changeFromValute(evt.target.value);
+                ConverteTorFnc(toValute,evt.target.value, toValue, fromValue); 
+                   
+               }}  > 
+                <option value="USD">USD</option> 
+                <option value="RUB">RUB</option> 
+                <option value="EUR">EUR</option>
+            </select>
+            <input type="text" className='toconvert' value={fromValue}  onChange={(evt)=>
+             {
+                       changeFromValue(evt.target.value); 
+                       ConverteTorFnc(toValute, fromValute);
+             
+             }
+
+            }/>
+        </div>
+    </div>
+  )
+}
