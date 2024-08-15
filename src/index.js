593dbd8409cd7c8094b554195f2fed7ace88ad52
test <mayil_10@mail.ru>
2024-08-15 13:34:05 +0300

    wws

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-
-
-    ssssssddd
+    index
+    index
+    index
   </React.StrictMode>
 );
 
