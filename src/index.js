cf7f6f53583c519f74f436652441e6cf52bc5c4a
test <mayil_10@mail.ru>
2024-08-15 13:35:13 +0300

    wws

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    index
-    index
-    index
+    <App />ddddd
   </React.StrictMode>
 );
 
